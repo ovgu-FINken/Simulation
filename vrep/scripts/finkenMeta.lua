ac8fb2c1c15492eb7059ffe562ad982199a6a9ec
lmaeurer <lmaeurer@st.ovgu.de>
2015-10-21 13:23:51 +0200

    ready for Simulation project forks

---
+++ vrep/scripts/finkenMeta.lua
@@ -0,0 +1,12 @@
+
+--[[
+--create a new finken by copying an old one to a new position
+--@return object_handle
+--]]
+function copyFinken(sourceFinken, positionTarget)
+	local handle_SourceFinkenTarget=simGetObjectHandle('SimFinken_target')
+	local handle_TargetFinken = simCopyPasteObjects({sourceFinken,handle_SourceFinkenTarget},2)
+	simSetObjectPosition(handle_TargetFinken[1], -1, positionTarget)
+	simSetObjectPosition(handle_TargetFinken[2], -1, positionTarget)
+	return handle_TargetFinken
+end
