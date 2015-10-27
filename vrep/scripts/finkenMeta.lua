8905f6ed3f45293230926586d74542585a1dd2c8
lmaeurer <lmaeurer@st.ovgu.de>
2015-10-27 03:12:30 +0100

    fixing finkenCopy

--- vrep/scripts/finkenMeta.lua
+++ vrep/scripts/finkenMeta.lua
@@ -1,7 +1,7 @@
 
 --[[
 --create a new finken by copying an old one to a new position
---@return object_handle
+--@return table object_handle
 --]]
 function copyFinken(sourceFinken, positionTarget)
 	local sourceFinkenSuffix, _ = simGetNameSuffix(sourceFinken)
@@ -9,8 +9,11 @@ function copyFinken(sourceFinken, positionTarget)
 	simSetNameSuffix(sourceFinkenSuffix)
 	local handle_SourceFinkenTarget=simGetObjectHandle('SimFinken_target')
 	simSetNameSuffix(nameSuffix)
-	local handles_TargetFinkenTarget = simCopyPasteObjects({handle_SourceFinkenTarget},1)
+	local handles_TargetFinkenTarget = simCopyPasteObjects({handle_SourceFinkenTarget},0)
 	simSetObjectPosition(handles_TargetFinkenTarget[1], -1, positionTarget)
+	local handles_TargetFinken= simCopyPasteObjects({sourceFinken},1)
+	simSetObjectPosition(handles_TargetFinken[1], -1, positionTarget)
 
-	return handles_TargetFinken
+	
+	return {handles_TargetFinkenTarget[1], handles_TargetFinken[1]} 
 end
