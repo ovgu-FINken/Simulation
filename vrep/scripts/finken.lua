eb2e4780319bf0c0b4cb402b1a858a9bf4caf6ab
lmaeurer <lmaeurer@st.ovgu.de>
2015-11-04 01:50:43 +0100

    write arena position to log file, log not sorted yet

--- vrep/scripts/finken.lua
+++ vrep/scripts/finken.lua
@@ -2,8 +2,22 @@ local finken = {}
 
 finkenCore = require('finkenCore')
 
-function finken.init(self)
 
+local function saveLog(newLog,newDirectoryPath)
+	local myTimeString = os.date("%Y%m%d%H%M%S")
+	newDirectoryPath = newDirectoryPath or ""
+	local myLogFile = assert(io.open(newDirectoryPath.."simulation" .. myTimeString .. ".log", "w"))
+	local timestamp, positionLogValue 
+	for timestamp, positionLogValue in pairs(newLog) do
+		myLogFile:write(timestamp, ": ", positionLogValue[1], " ", positionLogValue[2]," ", positionLogValue[3], "\n")
+		myLogFile:flush()
+	end
+	return myLogFile:close()
+end
+
+function finken.init(self)
+	local positionLog = {}
+	
 	local function helperSay(textToSay)
 		simAddStatusbarMessage(textToSay)
 	end
@@ -19,6 +33,19 @@ function finken.init(self)
 		helperSay("Hello World. Tschieep!")
 	end
 
+
+	--function customRun should be called in the vrep child script in the actuation part
+	--put here any custom function that should be called each simulation time step
+	function self.customRun()
+		local timestamp = math.floor(simGetSimulationTime()*1000)
+		positionLog[timestamp] = self.getArenaPosition()
+	end
+	
+	--function customClean should be called in the vrep child scrip in the cleanup part
+	--put here any custom function that should be called at the end of the simulation
+	function self.customClean()
+		saveLog(positionLog)
+	end
 	return self
 end
 
