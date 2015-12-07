40f4030a7c33cde9545837de4810932e3cef6b3e
gaetjen <johannesgaetjen@gmail.com>
2015-12-07 20:43:43 +0100

    imu can now have noise

--- finken_control/imu.lua
+++ finken_control/imu.lua
@@ -1,3 +1,5 @@
+require('math')
+
 local IMU = {}
 local objHandle = -1
 local velocity = {0, 0, 0}
@@ -11,8 +13,21 @@ function IMU.init(self, handle)
 		velocity, angularRate = simGetObjectVelocity(objHandle)
 		orientation = simGetObjectOrientation(objHandle, -1)
 
-		--TODO: add noise
+		sigma = simGetFloatSignal('imu:noiseMagnitude') or 0
 
+		-- apply noise
+		if sigma ~= 0 then
+			for i, v in ipairs(velocity) do
+				velocity[i] = v + pseudoNormalRandom(sigma)
+			end
+			--use lower noise values for angular velocity and orientation
+			for i, ar in ipairs(angularRate) do
+				angularRate[i] = ar + pseudoNormalRandom(sigma/180)
+			end
+			for i, o in ipairs(orientation) do
+				orientation[i] = o + pseudoNormalRandom(sigma/180)
+			end	
+		end		
 		--velocity x,y,z, roll, pitch (against world), yaw cannot be measured reliably in real copter, roll-, pitch-, yawrate
 		simSetFloatSignal('imu:xVel', velocity[1])
 		simSetFloatSignal('imu:yVel', velocity[2])
@@ -26,8 +41,17 @@ function IMU.init(self, handle)
 		--optional: pack multiple values in signal
 	end
 
-	return self
+	-- Generates random numbers that are approximately normally distributed, with mean 0 and standard deviation ~sigma
+	function pseudoNormalRandom(sigma)
+		sigma = sigma or 1
+		-- subtract from 1 because result can be 0, and we want to take the logarithm
+		local r = 1 - math.random()
+		-- devide by 1.81 to get approximately standard deviation 1
+		return sigma * math.log((1-r)/r) / 1.81
+	end
+
 	simAddStatusbarMessage('initialized IMU')
+	return self
 end
 
 return IMU
