824c3b7f55d5fc853ca97b91a6eb05afb9beb93d
Asema Hassan <asema.hassan@gmail.com>
2016-01-06 23:41:49 +0100

    Finked Local Map updated
    DONE
    
    -Set target position to the center of landscape.
    -Set finken position also to the center of landscape.
    -Create a 2D plane around the Finken and set it as child of Finken.
    -Stop dynamic movement of Finken, set it to static.
    -A UI to change the Size of map, and size of field.
    -Store data of local map in a 2D Array, which contains RGB value as
    vector.
    
     TODO:
    -Update this data per frame whenever the previous data is different
    from the current.
    -When local map data has filled the whole size of map. Then look for
    the target height on the landscape.

--- finken_control/finken_simpleGradient_localMap.lua
+++ finken_control/finken_simpleGradient_localMap.lua
@@ -1,6 +1,6 @@
 local finken = {}
 
-local boxContainer
+local boxContainer, sizeOfContainer,sizeOfField, resolutionOfMap,localMapDataTable = {n=1000}, row, col, xRes, yRes,midIndex
 
 finkenCore = require('finkenCore')
 
@@ -12,44 +12,116 @@ function finken.init(self)
 
 	function self.customInit()
 		helperSay("First task, Create gradient follower copter behavior ")
-	end
-
+    end
 
     -- Region # New methods for local map
 
     function self.CopterPositionSetToCenterOfMap()
         -- Set copter initial position in the center of map
-        simSetObjectPosition(simGetObjectHandle('SimFinken_base'), -1, {0,0,0})
+        simSetObjectPosition(simGetObjectHandle('FINken2'), -1, {0,0,0.5})
+    end
+
+    function self.SetTargetPositionToCenterOfMap()
+        --For testing of local map
+        simSetObjectPosition(targetObj, -1, {0, 0, 0})
+        self.setTarget(targetObj)
     end
 
     function self.CreateAVirtualBoxAroundFinken()
-
-        sizeOfContainer=1
         dupTolerance =0.0
         parentObjectHandle = simGetObjectHandle('SimFinken_base')
         maxItemCount = 1
 
-        boxContainer=simAddDrawingObject(sim_drawing_cubepoints+sim_drawing_itemsizes+sim_drawing_wireframe,sizeOfContainer,dupTolerance,parentObjectHandle,maxItemCount)
+        boxContainer=simAddDrawingObject(sim_drawing_quadpoints+sim_drawing_itemsizes+sim_drawing_wireframe,sizeOfContainer,dupTolerance,parentObjectHandle,maxItemCount)
 
         finkenSize=simGetObjectSizeFactor(simGetObjectHandle('SimFinken_base'))
         finkenCurrentPos=simGetObjectPosition(simGetObjectHandle('SimFinken_base'),-1)
 
-    --sim_drawing_cubepoints items are "cube points" (6 values per item (x;y;z;Nx;Ny;Nz) (N=normal vector)) + auxiliary values)
-        itemData={finkenCurrentPos[1],finkenCurrentPos[2],finkenCurrentPos[3],0,0,1,0.25*finkenSize}
+        --sim_drawing_quadpoints items are "rectangle points"
+        --(6 values per item (x;y;z;Nx;Ny;Nz) (N=normal vector)) + auxiliary values)
+        -- itemData = {position (x,y,z), normal(x,y,z), sizeOfContainer}
+        -- sizeOfContainer (0.05 to 1.0) meters, but if user enters in centimeters (5,100) then sizeOfContainer/100
 
+        itemData={finkenCurrentPos[1],finkenCurrentPos[2],finkenCurrentPos[3],0,0,1,sizeOfContainer/100}
         simAddDrawingObjectItem(boxContainer,itemData)
     end
 
+    function self.UpdateLocalMapDataFromUI()
+        sizeOfContainer =  simGetFloatSignal('_LM_SizeOfContainer')
+        sizeOfField = simGetFloatSignal('_LM_SizeOfField')
+        simAddStatusbarMessage('Map: '..sizeOfContainer..'  Field: '..sizeOfField)
+    end
+
+
+    function self.CalculateResolutionFromUserData()
+-- User specifies the size of container and field size (each block in cm's)
+        -- Calculate how many fields can fit into the size of container, which will define the resolution of local map
+
+        resolutionOfMap = (sizeOfContainer*sizeOfField)/100 --resolution in meters, math.floor()
+        simAddStatusbarMessage('ResolutionMap:  '..resolutionOfMap)
+
+        --to get array size
+        xRes = resolutionOfMap*100
+        yRes = resolutionOfMap*100
+
+        simAddStatusbarMessage('SizeOfMap X: '..xRes..' Y: '..yRes)
+
+        localMapDataTable = {n=yRes}
+        row =1
+        col =1
+    end
+
+    function self.CreateLocalMapDataTable()
+        --Set the default value at midSize of array to be the current position of Finken
+        -- Lets say, localMapDataTable[(xRes/2) * yRes + (yRes/2)] = {0,0}
+        for r=1,xRes do
+            for c=1,yRes do
+                        localMapDataTable[r*yRes + c] = {0,0}
+                    end
+            end
+
+        --Set center value of array to be, where copter is initially positioned, sample value {1,1}
+        midIndex =((xRes/2) * yRes + (yRes/2))
+        localMapDataTable[midIndex] = {1,1}
+    end
+
+
+    function self.PrintLocalMapDataTable()
+        for r=1,xRes do
+            for c=1,yRes do
+                simAddStatusbarMessage('AtIndex: '..(r*yRes + c)..' X: '..localMapDataTable[r*yRes + c][1]..', Y:'..localMapDataTable[r*yRes + c][2])
+            end
+        end
+    end
+
     function self.UpdateLocalMapWithColorSensorValue()
+
         local _, colors = simReadVisionSensor(simGetObjectHandle('Floor_camera'))
-
         --colors[2] is Red, colors[3] is Green, colors[4] is Blue
-        xPos = (colors[3] - 0.5) * speedFactor
-        yPos = ((colors[4] - 0.5) * -1) * speedFactor
-        zPos = ((colors[2] + 0.5)) * speedFactor
+        r = colors[2]
+        g = colors[3]
+        b = colors[4]
 
-        simSetObjectPosition(boxContainer,{xPos,yPos,zPos})
+        colorValue = {g,b}
 
+        --get xSpeed of map and shift index of localMap data accoridng to that.
+        xSpeed=simGetFloatSignal('_xSpeed')
+
+        if(xSpeed > 0) then
+            --shift array index to +1 and save prev. value to the new Index
+            midIndex = midIndex+1
+            localMapDataTable[midIndex] = colorValue
+        end
+
+        if(xSpeed < 0) then
+            --shift array index to -1 and save prev. value to the new Index
+            midIndex = midIndex-1
+              localMapDataTable[midIndex] = colorValue
+        end
+
+        if(xSpeed==0) then
+            self.PrintLocalMapDataTable()
+        end
     end
 
     -- end of new methods region
@@ -60,20 +132,16 @@ function finken.init(self)
         -- R: height
         -- G: x gradient (0.5 intensity corresponds to 0 gradient)
         -- B: y gradient
-
         -- the vision sensor Floor_camera returns a table of values
         -- the first element colors[1] is the overall lightness of the image
         -- the other elements colors[2] is Red, colors[3] is Green, colors[4] is Blue
 
 		local _, colors = simReadVisionSensor(simGetObjectHandle('Floor_camera'))
 		--simAddStatusbarMessage(colors[1]..' '..colors[2]..' '..colors[3]..' '..colors[4])
-		speedFactor = 1.5
 
-            --For hill.png gradient
-            -- passing colors[3] Green to xGrad.
+            speedFactor = 1.5
             xGrad = (colors[3] - 0.5) * speedFactor
 
-            -- passing colors[4] Blue to yGrad.
             --multiply with -1 because image coordinates start in top right
             yGrad = ((colors[4] - 0.5) * -1) * speedFactor
 
@@ -90,9 +158,10 @@ function finken.init(self)
             --keeping the Z value same as current target position, for hill.png gradient
             simSetObjectPosition(targetObj, -1, {xTarget, yTarget,zTarget})
 
-		--targetObject is retrieved in the simulation script. 
-		--remove if control via pitch/roll/yaw is wanted
-		self.setTarget(targetObj)
+            --targetObject is retrieved in the simulation script.
+            --remove if control via pitch/roll/yaw is wanted
+
+            self.setTarget(targetObj)
 	end
 
 	function self.customSense()
