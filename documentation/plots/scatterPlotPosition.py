1663585d84736e0c4cdd1cd61eb3d44e882a3fb3
lmaeurer <lmaeurer@st.ovgu.de>
2016-02-09 15:20:13 +0100

    pitch description in evaluation

--- documentation/plots/scatterPlotPosition.py
+++ documentation/plots/scatterPlotPosition.py
@@ -14,5 +14,17 @@ def plotpath(logFileName):
     #ax1.set_xlim([-4,0])
     fig.savefig('pos.png')
 
-plotpath('simulationPosition20151216140935.log')
+def plotLine(logFileName):
+    data = np.genfromtxt(logFileName, delimiter=' ', names=['time', 'pitch', 'roll', 'yaw'])
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(111)
+    ax1.plot(data['time'], data['roll'], color='r') 
+    ax1.plot(data['time'], data['pitch'], color='g')
+    #ax1.set_ylim([-10,3])
+    #ax1.set_xlim([-4,0])
+    fig.savefig('orientation.png')
+
+plotLine('simulation-1Orientation20160202225728.log')
+#plotpath('simulationPosition20151216140935.log')
 #plotpath('logExampleArenaBoundaries.log')    
