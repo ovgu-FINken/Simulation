cc820f12123d37454e8d17ce4ba2672f12a8b624
Christoph Steup <steup@ivs.cs.ovgu.de>
2017-05-11 00:23:28 +0200

    Fix library not loading bug
            - Crashes VREP on startup now

--- vrep/scripts/finkenPID.cpp
+++ vrep/scripts/finkenPID.cpp
@@ -1,40 +1,38 @@
 #include "finkenPID.h"
 
-class finkenPID {
-	finkenPID::finkenPID() {
-		p = 0;
-		i = 0;
-		d = 0;
-		e = 0;
-		e_prev = 0;
-		e_sum = 0;
-	}
+finkenPID::finkenPID() {
+  p = 0;
+  i = 0;
+  d = 0;
+  e = 0;
+  e_prev = 0;
+  e_sum = 0;
+}
 
-	int sign(float x) {
-		return (x<0) ? -1 : 1;
-	}
+int sign(float x) {
+  return (x<0) ? -1 : 1;
+}
 
-	void finkenPID::initPID(float newP, float newI, float newD){
-		p = newP;
-		i = newI;
-		d = newD;
-		e = 0;
-		e_prev = 0;
-		e_sum = 0;
-	}
+void finkenPID::initPID(float newP, float newI, float newD){
+  p = newP;
+  i = newI;
+  d = newD;
+  e = 0;
+  e_prev = 0;
+  e_sum = 0;
+}
 
-	float finkenPID::step(float newE, float deltaT){
-		e_prev = e;
-		e = newE;
-		if (sign(newE) == sign(e_prev)){
-			e_sum = e_sum + newE * deltaT;
-		}
-		else {
-			e_sum = 0;
-		}
-		return (p*e + d*((e-e_prev)/deltaT) + i*e_sum);
-	}
-};
+float finkenPID::step(float newE, float deltaT){
+  e_prev = e;
+  e = newE;
+  if (sign(newE) == sign(e_prev)){
+    e_sum = e_sum + newE * deltaT;
+  }
+  else {
+    e_sum = 0;
+  }
+  return (p*e + d*((e-e_prev)/deltaT) + i*e_sum);
+}
 
 int main(){
 	return 0;
