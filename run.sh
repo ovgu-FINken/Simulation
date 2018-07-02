c7041803ac9b3b636bd775992e6ae3ac606c2a64
Christoph Steup <steup@ivs.cs.ovgu.de>
2018-07-02 19:16:11 +0200

    Add Jupyter-Notebook for Sim analysis

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source vEnv/bin/activate
+jupyter-notebook Finken-Sim-Analysis.ipynb
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+virtualenv --python=python2.7 vEnv
+source vEnv/bin/activate
+pip install numpy-quaternion
+pip install ipykernel
+python -m ipykernel install --user --name testenv --display-name "Python2.7 Finken Sim Analysis"
